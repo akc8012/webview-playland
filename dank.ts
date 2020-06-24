9931027830989320f4b75a07d3a71045c5e4d11a
Andrew Colannino <akcolannino@email.neit.edu>
2020-06-23 21:55:56 -0400

    Through some twisted miracle, Bulma CSS actually works

--- dank.ts
+++ dank.ts
@@ -1,17 +1,29 @@
-import { WebView } from "https://deno.land/x/webview/mod.ts";
+import { WebView } from 'https://deno.land/x/webview/mod.ts';
 
 let html = `
 	<html>
-		<body>
-			<h1>Hello friends!</h1>
-			<button>Ass!</button>
-		</body>
+	<head>
+		<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css'>
+	</head>
+	<body>
+	<section class='section'>
+		<div class='container'>
+		<h1 class='title'>
+			Hello friends!
+		</h1>
+		<p class='subtitle'>
+			Press the ass button... <strong>If you dare</strong>!
+		</p>
+		<button class='button is-danger is-light'>Ass!</button>
+		</div>
+	</section>
+	</body>
 	</html>
 `;
 
 const webview = new WebView({
-	title: "Deno Webview Example",
-	url: "data:text/html," + html,
+	title: 'Deno Webview Example',
+	url: 'data:text/html,' + html,
 	width: 800,
 	height: 600,
 	resizable: true,
